670e6755f6293dfc2c70806430aded21d07ce107
Caio de Lima Granero <caio.granero@livance.com.br>
2019-06-19 13:28:49 -0300

    Fiz a tabelinha com as atividades praticadas do aluno

--- src/views/Avaliacoes.vue
+++ src/views/Avaliacoes.vue
@@ -1,33 +1,41 @@
 <template>
-  <div id="treinos">
+  <div id="avaliacoes">
     <base-info></base-info>
     <br>
 
     <b-container>
       <b-row class="background-view">
-        <b-col md="6">
+        <b-col md="12">
           <br>
           <h5> Atividades praticadas </h5>
           <br>
+          <b-row>
+            <b-col md="4">
+              <b-form-group id="input-group-1" label="Atividade:" label-for="atividade">
+                <v-select v-model="form.atividades.atividade" :filterable="true" id="atividade" :clearable="true" :autocomplete="true" :options="atividades"></v-select>
+              </b-form-group>
+            </b-col>
+            <b-col md="2">
+              <b-form-group id="input-group-1" label="Frequencia" label-for="frequencia" description="vezes por semana">
+                <b-form-input id="frequencia" size="sm" type="number" v-model="form.atividades.frequencia" placeholder=""></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col md="2">
+              <b-form-group id="input-group-1" label="Tempo" label-for="tempo" description="meses">
+                <b-form-input id="tempo" size="sm" type="number" v-model="form.atividades.tempo" placeholder=""></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col md="2">
+              <b-form-group id="input-group-1" label="Duração" label-for="duracao" description="minutos">
+                <b-form-input id="duracao" size="sm" type="number" v-model="form.atividades.duracao" placeholder=""></b-form-input>
+              </b-form-group>
+            </b-col>  
+            <b-col>
+              <b-button type="submit" variant="primary" @click="adicionarAtividade()">ADICIONAR</b-button>
+            </b-col>
+          </b-row>
+          
           <b-table small hover :fields="fields" :items="items" :current-page="page.currentPage" :per-page="page.perPage">
-            <template slot="atividade" slot-scope="cell">
-              <v-select 
-                v-model="cell.item.atividade"
-                :filterable="true" 
-                :clearable="true"
-                :autocomplete="true"
-                :options="atividades">
-              </v-select>
-            </template>
-            <template slot="frequencia" slot-scope="cell">
-              <b-form-input id="input-small" size="sm" type="text" v-model="cell.item.frequencia" placeholder=""></b-form-input>
-            </template>
-            <template slot="tempo" slot-scope="cell">
-              <b-form-input id="input-small" size="sm" type="text" v-model="cell.item.tempo" placeholder=""></b-form-input>
-            </template>
-            <template slot="duracao" slot-scope="cell">
-              <b-form-input id="input-small" size="sm" type="text" v-model="cell.item.duracao" placeholder=""></b-form-input>
-            </template>
             <template slot="remover" slot-scope="cell">
               <b-button size="sm" @click="remove(cell.item)">
                 <i class="fas fa-minus-circle clickable"></i>
@@ -59,24 +67,28 @@ export default {
   components: { BaseInfo },
   data() {
     return {
-      fields: ["atividade", "frequencia", "tempo", "duracao", "remover"],
+      fields: [
+        { key: "atividade.label", label: "Atividade" },
+        "frequencia",
+        "tempo",
+        "duracao",
+        "remover"
+      ],
       atividades: [
         {
           id: 1,
           label: "Caio"
         }
       ],
-      items: [
-        {
-          atividade: {
-            id: 1,
-            label: "Caio"
-          },
-          frequencia: 2,
-          tempo: 4,
-          duracao: 5
+      form: {
+        atividades: {
+          frequencia: "",
+          tempo: "",
+          duracao: "",
+          atividade: null
         }
-      ],
+      },
+      items: [],
       page: {
         perPage: 4,
         currentPage: 0
@@ -89,9 +101,20 @@ export default {
     //   .then(response => response.data.data.atividades)
     //   .then(atividades => (this.atividades = atividades));
   },
-  methods: {}
+  methods: {
+    adicionarAtividade() {
+      this.items.push(this.form.atividades);
+    },
+    onSubmit() {},
+    remove(item) {
+      console.log(item);
+    }
+  }
 };
 </script>
 
 <style scoped>
+#avaliacoes .b-form-group {
+  text-align: left;
+}
 </style>
